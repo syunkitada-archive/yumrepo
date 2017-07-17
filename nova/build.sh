c7f3d86c7d8e0360b6def433ab8a2c9cb3a5ce91
Shunya Kitada <syun.kitada@gmail.com>
2017-07-17 22:44:28 +0900

    update packages

--- nova/build.sh
+++ nova/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -xe
 
 BASE_DIR=/tmp/base/
-NAME=nova
-VERSION=$1
+NAME=$1
+VERSION_NAME=$2
+VERSION=$3
 RELEASE=`date +"%Y%m%d"`
 TOP_DIR=/tmp/rpmbuild
 SRC_DIR=$TOP_DIR/SOURCES
@@ -11,9 +12,10 @@ mkdir -p $SRC_DIR
 cd $BASE_DIR/../
 tar -cf $SRC_DIR/base.tar.gz base
 
-rpmbuild --bb base/rpm.spec \
+rpmbuild --bb base/${VERSION_NAME}-rpm.spec \
     --define "_topdir ${TOP_DIR}" \
     --define "name ${NAME}" \
+    --define "version_name ${VERSION_NAME}" \
     --define "version ${VERSION}" \
     --define "release ${RELEASE}"
 
