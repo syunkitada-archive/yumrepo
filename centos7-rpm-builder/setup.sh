4aa1f2e39874cc9e38d28067824534b955f7bb22
Shunya Kitada <syun.kitada@gmail.com>
2017-09-03 09:36:34 +0900

    update openstack packages

--- centos7-rpm-builder/setup.sh
+++ centos7-rpm-builder/setup.sh
@@ -4,7 +4,7 @@ yum -y update
 yum -y install epel-release
 yum -y install curl wget git gcc gcc-c++ rpm-build createrepo \
                libxml2-devel libxslt-devel libffi-devel \
-               openssl-devel libvirt-devel liberasurecode-devel \
+               openssl-devel liberasurecode-devel \
                pulseaudio-libs-devel systemtap-sdt-devel bluez-libs-devel \
                libusb1-devel vte3-devel librdmacm-devel \
                iasl libuuid-devel SDL-devel libaio-devel usbredir-devel \
